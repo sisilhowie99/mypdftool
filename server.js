eb1e9f24f00606f35645c5a05b1ee483e30e0c59
sisilhowie99 <sisil.anastasia@gmail.com>
2021-08-04 19:30:17 +1000

    Add server code

---
+++ server.js
@@ -0,0 +1,44 @@
+// Require the modules
+const express = require('express');
+// Instantiate express server
+const app = express();
+const multer = require('multer');
+const cors = require('cors');
+// Use CORS for the server
+app.use(cors());
+
+
+// Create Multer instance using diskStorage engine to save uploaded files to machine's file system
+const storage = multer.diskStorage({
+    destination: './public',
+    filename: function(req, file, cb) {
+        cb(null, `${file.originalname}`);
+    }
+})
+
+// upload instance handling a single file uploaded
+const upload = multer({ storage: storage }).single('uploaded_file');
+
+// POST route
+app.post('/upload', upload, function(req, res) {
+    // Upload instance 
+    upload(req, res, function(err) {
+        // Check for errors (MulterError first then other errors)
+        if(err instanceof multer.MulterError) {
+            console.log('There\'s a MulterError.');
+            return res.status(500).json(err);
+        } else if(err) {
+            console.log('There\'s another error (not MulterError).');
+            return res.status(500).json(err);
+        }
+
+        console.log(req.file);          // the file uploaded
+        console.log('Upload success');
+        return res.status(200).send(req.file);
+    })    
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+})
