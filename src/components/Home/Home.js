9b7bbe0b6b38403d4d910ac746e3dc39eb823ea7
sisilhowie99 <sisil.anastasia@gmail.com>
2021-08-11 21:51:13 +1000

    Add logo
    Add heading. Remove Upload from Home, use buttons for Upload and Create instead

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,30 @@
-import React from 'react';
-import Upload from '../Upload/Upload';
+import React from "react";
+
+import './Home.css';
+import logo from '../../resources/logo.png';
 
 class Home extends React.Component {
     render() {
         return (
-            <div className='home-container'>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">MyPDFtool</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="/">View</a>
-                                </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="/">Create</a>
-                                </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="/">Modify</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className='intro'>
-                    <p>Create fillable PDF form files or modify existing ones, and save them as a new one or overwrite the old one.</p>
+            <div className="container home-container">
+                <div className="intro">
+                    <img src={logo} alt='MyPDFtool logo' className='logo' />
+                    <h1>
+                        Create or modify fillable PDF files.<br />
+                        Overwrite the old one or create a new file.
+                    </h1>
                 </div>
-                <Upload />
+
+                <div className="d-grid gap-2 button-container">
+                    <a href="/upload" className="btn btn-primary" type="button">
+                        Upload
+                    </a>
+                    <a href="/create" className="btn btn-primary" type="button">
+                        Create
+                    </a>
+                </div>
             </div>
-        )
+        );
     }
 }
 
