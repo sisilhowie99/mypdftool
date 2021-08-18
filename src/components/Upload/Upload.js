e01543e8f3021aa0f54acbb53fd9a0d3266a534f
sisilhowie99 <sisil.anastasia@gmail.com>
2021-08-18 11:34:49 +1000

    Add state to check upload success status
    Use Router Redirect to redirect to Display if so, otherwise display nothing

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import axios from 'axios';
-
-// Import the uploaded file
-import file from './uploaded_file.pdf';
-import Display from '../Display/Display';
+import React from "react";
+import axios from "axios";
+import { Redirect } from 'react-router-dom';
 
 class Upload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            uploadedFile: null
+            uploadedFile: null,
+            isUploadSuccess: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
@@ -29,26 +27,32 @@ class Upload extends React.Component {
         const data = new FormData();
 
         // Add key/value pair to the object
-        data.append('uploaded_file', this.state.uploadedFile);
+        data.append("uploaded_file", this.state.uploadedFile);
 
         // Make POST request using Axios, sending the data object to the url
         // The request returns a promise
-        axios.post('http://localhost:8000/upload', data).then(res => {
+        axios.post("http://localhost:8000/upload", data).then((res) => {
             // If successful, print the status text of the response
             console.log(`The request is successful. Status code: ${res.status} - ${res.statusText}`);
+        });
+
+        // Call redirect function to update state, after the file's uploaded to server
+        this.redirect();
+    }
+
+    redirect() {
+        // Set state to indicate upload successful
+        this.setState({
+            isUploadSuccess: true
         })
     }
 
-
     render() {
         return (
-            <div className='container upload-container'>
-                <form
-                    method="POST"
-                    encType="multipart/form-data"
-                >
+            <div className="container upload-container">
+                <h1>Upload fillable PDF file</h1>
+                <form method="POST" encType="multipart/form-data">
                     <div className="mb-3">
-                        <label htmlFor="formFile" className="form-label">Upload PDF file</label>
                         <input
                             className="form-control"
                             type="file"
@@ -57,14 +61,25 @@ class Upload extends React.Component {
                             accept=".pdf"
                             onChange={this.handleChange}
                         />
-                        <div className="d-grid gap-2">
-                            <button className="btn btn-primary" type="button" onClick={this.handleClick}>Upload</button>
+                        <div
+                            className="d-grid gap-2 upload"
+                            style={{ margin: "20px auto" }}
+                        >
+                            <button
+                                className="btn btn-primary"
+                                type="button"
+                                onClick={this.handleClick}
+                            >
+                                Upload
+                            </button>
                         </div>
                     </div>
                 </form>
-                <Display uploadedFile={file} />
+
+                {/* Check if upload is successful then redirect user to Display, otherwise display empty string (nothing) */}
+                {this.state.isUploadSuccess ? <Redirect to='/display' /> : ""}
             </div>
-        )
+        );
     }
 }
 
