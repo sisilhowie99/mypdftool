4343e9c59ec6be1a22f348e4935ebeb5417d5833
sisilhowie99 <sisil.anastasia@gmail.com>
2021-08-25 11:11:06 +1000

    Add GET /modify
    Not fully working yet due to CORS policy - supposed to print response from server

--- src/components/Modify/Modify.js
+++ src/components/Modify/Modify.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 class Modify extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: null
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/modify').then(res => {
+            console.log(`in modify page - response: ${res.data}`);
+            // this.setState({
+            //     content: res
+            // })
+        })
+    }
     render() {
         return (
             <div className='container modify-container'>
